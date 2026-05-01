84e8808529f955f032a21cd8163c0e459200718d
Dumitru Diacenco <dumitrudiacenco@Dumitrus-MacBook-Pro.local>
2026-05-01 22:16:31 +0300

    feat: adjust persisted ticket price values

---
+++ backend/src/main/resources/db/migration/V4__update_prices_to_mdl.sql
@@ -0,0 +1,8 @@
+UPDATE prices SET amount = 80  WHERE category = 'STANDARD' AND format = 'TWO_D';
+UPDATE prices SET amount = 100 WHERE category = 'STANDARD' AND format = 'THREE_D';
+UPDATE prices SET amount = 120 WHERE category = 'VIP'      AND format = 'TWO_D';
+UPDATE prices SET amount = 150 WHERE category = 'VIP'      AND format = 'THREE_D';
+UPDATE prices SET amount = 60  WHERE category = 'CHILD'    AND format = 'TWO_D';
+UPDATE prices SET amount = 70  WHERE category = 'CHILD'    AND format = 'THREE_D';
+UPDATE prices SET amount = 70  WHERE category = 'STUDENT'  AND format = 'TWO_D';
+UPDATE prices SET amount = 85  WHERE category = 'STUDENT'  AND format = 'THREE_D';
